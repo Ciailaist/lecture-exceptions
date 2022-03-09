adc7dcfe2ec459ea7a88ee985420ef42bfbf0082
Tiago Sales <tgoprince@gmail.com>
2022-03-09 16:45:41 +0100

    Add solution to exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
 
   public static void main(String[] args) {
+    int[] ia = new int[15];
     try {
-      method1();
-    } catch (Exception e) {
-      System.err.printf("%s%n%n", e.getMessage());
-
-      StackTraceElement[] traceElements = e.getStackTrace();
-      System.out.printf("%nStack trace from getStrackTrace()%n");
-      System.out.println("Class\t\t\tFile\t\t\t\t\tLine\tMethod");
-      for (StackTraceElement traceElement : traceElements) {
-        System.out.printf("%s\t", traceElement.getClassName());
-        System.out.printf("%s\t\t", traceElement.getFileName());
-        System.out.printf("%02d\t\t", traceElement.getLineNumber());
-        System.out.printf("%s%n", traceElement.getMethodName());
+      for (int i = 0; i < 10; i++) {
+        ia[i] = i;
+        System.out.println(Arrays.toString(ia));
       }
-
-      e.printStackTrace();
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println(e.getMessage());
+      return;
     }
-  }
-
-  public static void method1() throws Exception {
-    System.out.println("Method 1 called");
-    method2();
-  }
-
-  public static void method2() throws Exception {
-    System.out.println("Method 2 called");
-    method3();
-  }
-
-  public static void method3() throws Exception {
-    System.out.println("Method 3 called");
-    throw new Exception("Exception thrown in method 3");
   }
 
 }
