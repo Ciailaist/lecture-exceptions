d944a703b1dddae6a9bb7414618e0bd60dcc66b7
Tiago Sales <tgoprince@gmail.com>
2022-03-09 14:39:44 +0100

    Add finally block on working example

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,31 @@ public class Main {
     boolean continueLoop = true; // determines if more input is needed
 
     while (continueLoop) {
+      Integer numerator, denominator, quotient = null;
+
       try { // read two numbers and calculate quotient
         System.out.print("Please enter an integer numerator: ");
-        int numerator = scanner.nextInt();
+        numerator = scanner.nextInt();
 
         System.out.print("Please enter an integer denominator: ");
-        int denominator = scanner.nextInt();
+        denominator = scanner.nextInt();
 
-        int quotient = quotient(numerator, denominator);
+        quotient = quotient(numerator, denominator);
         System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, quotient);
 
         continueLoop = false;
-      } catch (ArithmeticException | InputMismatchException e) {
-        e.printStackTrace();
+      } catch (InputMismatchException e) {
         scanner.nextLine(); // discard input so we can try again
-        System.out.printf("Either an ArithmeticException or an InputMismatchException was thrown. Please try again.%n");
+        e.printStackTrace();
+        System.out.printf("You must enter integers. Please try again.%n");
+      } catch (ArithmeticException e) {
+        e.printStackTrace();
+        System.out.printf("Cannot divide by 0. Please try again.%n");
+      } finally {
+        if (quotient != null)
+          System.out.printf("OK: Successful division!%n");
+        else
+          System.out.printf("ERROR: Unsuccessful division!%n");
       }
     }
   }
